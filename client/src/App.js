9ea1f090673ba1f404576eafa92e3cfc1ff44ff4
Kevin <stradtkt22@gmail.com>
2019-06-16 09:14:02 -0400

    Added the client

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div className="App">
+        <div className="container">
+            <h1>SpaceX</h1>
+        </div>
+    </div>
+  );
+}
+
+export default App;
